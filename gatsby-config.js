e09417730a77023ec002df24e8f58396662bf226
ghughes13 <guitarguy13@ymail.com>
2020-11-22 12:10:57 -0600

    added ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.tridigitalmarketing.com`,
+    siteUrl: `https://synthdigital.com/`,
     title: `SynthDigital`,
     description: `We’re a UI/UX strategy and design agency in Dallas, Tx. We create kickass digital products accross all platforms.`,
     author: `@ghughes139`,
@@ -8,14 +8,32 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sitemap`,
-    // {
-    //   resolve: "gatsby-plugin-robots-txt",
-    //   options: {
-    //     host: "https://www.tridigitalmarketing.com",
-    //     sitemap: "https://www.tridigitalmarketing.com/sitemap.xml",
-    //     policy: [{ userAgent: "*", allow: "/" }],
-    //   },
-    // },
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        host: "https://synthdigital.com/",
+        sitemap: "https://synthdigital.com/sitemap.xml",
+        policy: [{ userAgent: "*", allow: "/" }],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-82341804-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        defer: false,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
