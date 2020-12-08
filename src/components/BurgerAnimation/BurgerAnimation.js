de1025751f5abe42ce5365c9e2466d8d60009f46
ghughes13 <guitarguy13@ymail.com>
2020-12-08 17:48:33 -0600

    fixed burger menu

--- src/components/BurgerAnimation/BurgerAnimation.js
+++ src/components/BurgerAnimation/BurgerAnimation.js
@@ -5,10 +5,23 @@ import "./BurgerAnimation.scss";
 const App = () => {
   const [isActive, setIsActive] = useState(false);
 
+  const setClass = state => {
+    if (state && document.querySelector(".opening")) {
+      console.log("hi");
+      document.querySelector(".opening").classList.add("open");
+    } else if (document.querySelector(".opening")) {
+      console.log("bye");
+      document.querySelector(".opening").classList.remove("open");
+    }
+  };
+
   const toggleButton = useCallback(() => {
-    setIsActive(prevState => !prevState);
     document.getElementById("burger-btn").click();
+    setIsActive(prevState => !prevState);
   }, []);
+
+  console.log(isActive);
+  setClass(isActive);
 
   return (
     <HamburgerSlider
